adfb28bc9f5f1ab4196cc54bbecb85fd0ae276ef
bigtyno <bigtyno4@gmail.com>
2022-06-16 18:18:56 +0900

    Initial commit

---
+++ src/Bj1110.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 더하기 사이클 
+public class Bj1110 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
