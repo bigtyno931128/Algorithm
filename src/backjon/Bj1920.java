ead0dab30b0f7854d940b70487584820dfef59cf
bigtyno <bigtyno4@gmail.com>
2022-07-04 23:07:28 +0900

    .

---
+++ src/backjon/Bj1920.java
@@ -0,0 +1,22 @@
+package backjon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 수 찾기
+public class Bj1920 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+
+
+    }
+}
